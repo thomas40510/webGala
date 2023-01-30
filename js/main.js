c21f1ba442b680be6c5650779de96a062f72f45c
Thomas Prévost <thomasprevost85@gmail.com>
2023-01-30 09:15:26 +0100

    update countdown text

--- js/main.js
+++ js/main.js
@@ -299,10 +299,10 @@
 		// + minutes + "Minutes " + seconds + "Seconds ";
 
 		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>days</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
+		document.getElementById("days").innerHTML = days +" <small>jours</small>";
+		document.getElementById("hours").innerHTML = hours + " <small>heures</small> ";
 		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+		document.getElementById("seconds").innerHTML = seconds + " <small>secondes</small> ";
 
 		// If the count down is finished, write some text 
 		if (distance < 0) {
